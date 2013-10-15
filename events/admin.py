98c1c1640f61ccfbf370c7b70b05445898537115
Rafael Tovar <hola@rafaeltovar.info>
2013-10-15 21:16:38 +0200

    Arreglos pequeños

--- events/admin.py
+++ events/admin.py
@@ -14,13 +14,11 @@ class PropertyAdmin(admin.ModelAdmin):
 class EventAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     list_display = ('name', 'to_slug')
+    inlines = [PropertyInline, ]
     
     def to_slug(self, obj):
         return ("/%s" % (obj.slug))
     to_slug.short_description = 'Slug'
-	
-admin.site.register(Event,EventAdmin)
-    inlines = [PropertyInline, ]
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Property, PropertyAdmin)
--- events/models.py
+++ events/models.py
@@ -17,18 +17,6 @@ class Property(models.Model):
 
 
 class Event(models.Model):
-<<<<<<< HEAD
-	STATUS = ((0, 'Close'), (1, 'Open'), (2, 'Hidden'), (3, 'Draft'))
-	
-	name = models.CharField(max_length=250)
-	description = models.CharField(max_length=500)
-	status = models.IntegerField(max_length=1, choices=STATUS, null=False, default=0)
-	slug = models.CharField(max_length=250)
-	created = models.DateTimeField(default=datetime.now(), null=True, blank=True)
-
-	def __unicode__(self):
-	   return u'%s (%s)' % (self.name, self.description)
-=======
     STATUS = ((0, 'Close'), (1, 'Open'), (2, 'Hidden'), (3, 'Draft'))
 
     name = models.CharField(max_length=250)
@@ -43,4 +31,3 @@ class PropertyEvent(models.Model):
     event_id = models.ForeignKey(Event)
     property_id = models.ForeignKey(Property)
     status = models.IntegerField(max_length=1, choices=settings.CONTENT_STATUS, null=False, default=1)
->>>>>>> 76967f5957e2278b329ac3920f2b0afa8106376f
